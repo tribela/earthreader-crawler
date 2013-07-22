4a93f1e16109f1aa29a6da750ebd72e724b90545
kjwon15 <kjwonmail@gmail.com>
2013-07-22 13:20:51 +0900

    added function which parse rss feed

---
+++ parser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib2
+
+def parse_rss(content):
+    soup = BeautifulSoup(content, 'xml')
+
+    title = soup.title.text
+    link = soup.link.text
+
+    print "%s(%s)" % (title, link)
+
+    items = soup('item')
+    for item in items:
+        title = item.title.text
+        link = item.link.text
+        uid = item.guid.text
+
+        print " %s(%s) - (%s)" % (title, link, uid)
+
+def parse_atom(content):
+    pass
+
+def parse_feed(feed_type, url):
+    try:
+        content = urllib2.urlopen(url).read()
+        if feed_type in ['rss', 'rss+xml']:
+            parse_rss(content)
+        elif feed_type in ['atom', 'atom+xml']:
+            parse_atom(content)
+    except urllib2.URLError, e:
+        return None
+
+if __name__ == '__main__':
+    feed_type = raw_input('Input type: ')
+    url = raw_input('Input url: ')
+
+    parse_feed(feed_type, url)
